7211ff0efce60f4eb64879405311693ba3771101
Jecha Tomas <jecha@avast.com>
2019-08-26 12:46:12 +0200

    Fixed script netver.ps1 for single line Ctrl+C&V

--- scripts/netver.ps1
+++ scripts/netver.ps1
@@ -1,8 +1,5 @@
-﻿# This script returns installed version of .NET 4.*
-# Latest version: https://github.com/jechtom/global/blob/master/scripts/netver.ps1
-# Data source: https://github.com/dotnet/docs/blob/master/docs/framework/migration-guide/how-to-determine-which-net-framework-updates-are-installed.md
-
-$release = (Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release
+﻿
+$release = (Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release;
 $versions = @{
 ".NET Framework 4.5"   = 378389;
 ".NET Framework 4.5.1" = 378758;
@@ -14,7 +11,7 @@ $versions = @{
 ".NET Framework 4.7.1" = 461310;
 ".NET Framework 4.7.2" = 461814;
 ".NET Framework 4.8"   = 528049
-}
+};
 $versions.Keys | 
     % { [PSCustomObject]@{Name=$_; Release=$versions[$_]; Installed=$release; IsPresent=$release -ge $versions[$_]} } | 
     Sort-Object -Property Release | Format-Table -AutoSize
