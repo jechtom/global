e8b399698b2bb320aa534a52956adc318761bf68
Jecha Tomas <jecha@avast.com>
2019-08-29 12:13:38 +0200

    Fixed .NET version detection - changed treshold to lowest version for each framework - closes issue #1

--- scripts/netver.ps1
+++ scripts/netver.ps1
@@ -2,15 +2,15 @@
 $release = (Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release;
 $versions = @{
 ".NET Framework 4.5"   = 378389;
-".NET Framework 4.5.1" = 378758;
+".NET Framework 4.5.1" = 378675;
 ".NET Framework 4.5.2" = 379893;
-".NET Framework 4.6"   = 393297;
-".NET Framework 4.6.1" = 394271;
-".NET Framework 4.6.2" = 394806;
-".NET Framework 4.7"   = 460805;
-".NET Framework 4.7.1" = 461310;
-".NET Framework 4.7.2" = 461814;
-".NET Framework 4.8"   = 528049
+".NET Framework 4.6"   = 393295;
+".NET Framework 4.6.1" = 394254;
+".NET Framework 4.6.2" = 394802;
+".NET Framework 4.7"   = 460798;
+".NET Framework 4.7.1" = 461308;
+".NET Framework 4.7.2" = 461808;
+".NET Framework 4.8"   = 528040
 };
 $versions.Keys | 
     % { [PSCustomObject]@{Name=$_; Release=$versions[$_]; Installed=$release; IsPresent=$release -ge $versions[$_]} } | 
